355a1d574e0a4a1d93555478555f808adeae5c26
PNS212624\Hansol <yrlee00@hansol.com>
2023-01-26 09:49:23 +0900

    1/26 commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("문자열1");
+        stringBuilder.append("문자열2");
+        String str = stringBuilder.toString();
+        System.out.println(str);
+
+    }
+}
